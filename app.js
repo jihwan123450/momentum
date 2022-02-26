517df33cbe7035dd92d2bcf85473f1e43e0d0825
jihwan123450 <99623807+jihwan123450@users.noreply.github.com>
2022-02-26 20:43:27 +0900

    ch#4

---
+++ app.js
@@ -0,0 +1,36 @@
+const loginInput = document.querySelector(".form input");
+const loginForm = document.querySelector(".form ");
+const header = document.querySelector("h1");
+const HIDDEN_CLASSNAME = "hidden";
+const savedUsername = localStorage.getItem("username");
+
+function loginSubmit(event) {
+    event.preventDefault();
+    const username = loginInput.value;
+    loginForm
+        .classList
+        .add(HIDDEN_CLASSNAME)
+    header
+        .classList
+        .remove(HIDDEN_CLASSNAME);
+    localStorage.setItem("username", username);
+    header.innerText = `Hello ${username}`;
+
+}
+function greetings(username) {
+
+    loginForm
+        .classList
+        .add(HIDDEN_CLASSNAME)
+    header
+        .classList
+        .remove(HIDDEN_CLASSNAME);
+    header.innerText = `Hello ${username}`;
+}
+
+if (savedUsername === null) {
+    loginForm.addEventListener("submit", loginSubmit);
+
+} else {
+    greetings(savedUsername);
+}
